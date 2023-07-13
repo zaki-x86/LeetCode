5834e1eaa1051b006008ff47d714b11b248dfa98
M Zaki <55619526+zaki-x86@users.noreply.github.com>
2023-07-13 13:14:41 +0300

    Time: 25 ms (16.59%), Space: 5.8 MB (82.37%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -2,18 +2,9 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         std::string s = std::to_string(x);
-        if (s.size() == 1)
-            return true;
-        if (s.size() == 2 && s[0] != s[1])
-            return false;
-        auto last_index = s.size() - 1;
-        auto mid_index = s.size() % 2 == 0 ?
-              (s.size() / 2) - 1
-            : static_cast<int>(s.size() / 2);
-        if (s.size() % 2 == 0 && s[mid_index] != s[mid_index + 1])
-            return false;
-        for (size_t i = 0; i < mid_index; ++i)
-            if (s[i] != s[last_index - i])
+        
+        for (size_t i = 0; i < s.size() / 2; ++i)
+            if (s[i] != s[s.size() - 1 - i])
                 return false;
         return true;
     }
